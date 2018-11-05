bb7284fd7c9bda35d9b1423eb8d72ef9bd59987f
wullparker <william.parker@hsbc.com>
2018-11-05 21:18:42 +0000

    Initial commit

---
+++ src/main/java/com/porkerspicks/PorkersPicksApplication.java
@@ -0,0 +1,12 @@
+package com.porkerspicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorkersPicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorkersPicksApplication.class, args);
+	}
+}
